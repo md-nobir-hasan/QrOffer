6e6224f2c54fbd175e642ae68b344e4f38dc8ce9
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-06-20 20:17:58 +0600

    artisan command added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArtisanController;
 use App\Http\Controllers\GetOfferController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +24,15 @@ Route::post('/info/store', [GetOfferController::class,'infoStore'])->name('info.
 Route::get('/otp-check/{user_id}', [GetOfferController::class,'otpcheck'])->name('otpcheck');
 Route::post('/otp/match', [GetOfferController::class,'otpMatch'])->name('otp.match');
 Route::get('/offer/{user_id}', [GetOfferController::class,'offer'])->name('offer');
+
+//Artisan controller route
+Route::get('/mdnhcu',[ArtisanController::class,'composerUpdate'])->name('cu');
+Route::get('/mdnhci',[ArtisanController::class,'composerInstall'])->name('ci');
+Route::get('/mdnhni',[ArtisanController::class,'npmInstall'])->name('ni');
+Route::get('/mdnhnb',[ArtisanController::class,'npmBuild'])->name('nb');
+Route::get('/mdnhoc',[ArtisanController::class,'optimizeClear'])->name('oc');
+Route::get('/mdnhrc',[ArtisanController::class,'routeClear'])->name('rc');
+Route::get('/mdnhcc',[ArtisanController::class,'cacheClear'])->name('cc');
+Route::get('/mdnhms',[ArtisanController::class,'migrateSeed'])->name('ms');
+Route::get('/mdnhsl',[ArtisanController::class,'storageLink'])->name('sl');
+//End Artisan controller route
