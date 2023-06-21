4533373639c0d7068c45e560a9bfac525e8be4a2
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-06-21 15:37:50 +0600

    vendor added

---
+++ vendor/xenon/laravelbdsms/src/Provider/BulkSmsBD.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ *  Last Modified: 6/29/21, 12:06 AM
+ *  Copyright (c) 2021
+ *  -created by Ariful Islam
+ *  -All Rights Preserved By
+ *  -If you have any query then knock me at
+ *  arif98741@gmail.com
+ *  See my profile @ https://github.com/arif98741
+ */
+
+namespace Xenon\LaravelBDSms\Provider;
+
+use GuzzleHttp\Exception\GuzzleException;
+use Xenon\LaravelBDSms\Handler\ParameterException;
+use Xenon\LaravelBDSms\Handler\RenderException;
+use Xenon\LaravelBDSms\Request;
+use Xenon\LaravelBDSms\Sender;
+
+class BulkSmsBD extends AbstractProvider
+{
+    private string $apiEndpoint = 'https://bulksmsbd.net/api/smsapi';
+    /**
+     * BulkSmsBD constructor.
+     * @param Sender $sender
+     */
+    public function __construct(Sender $sender)
+    {
+        $this->senderObject = $sender;
+    }
+
+    /**
+     * Send Request To Api and Send Message
+     * @throws GuzzleException|RenderException
+     */
+    public function sendRequest()
+    {
+        $number = $this->senderObject->getMobile();
+        $text = $this->senderObject->getMessage();
+        $config = $this->senderObject->getConfig();
+        $queue = $this->senderObject->getQueue();
+
+        $query = [
+            'api_key' => $config['api_key'],
+            'senderid' => $config['senderid'],
+            'type' => 'text',
+            'number' => $number,
+            'message' => $text,
+        ];
+
+        if (array_key_exists('senderid', $config)) {
+            $query ['senderid'] = $config['senderid'];
+        }
+
+        $requestObject = new Request($this->apiEndpoint, $query, $queue);
+        $response = $requestObject->get();
+        if ($queue) {
+            return true;
+        }
+
+        $body = $response->getBody();
+        $smsResult = $body->getContents();
+
+        $data['number'] = $number;
+        $data['message'] = $text;
+        return $this->generateReport($smsResult, $data)->getContent();
+    }
+
+    /**
+     * @throws ParameterException
+     */
+    public function errorException()
+    {
+        if (!array_key_exists('api_key', $this->senderObject->getConfig())) {
+            throw new ParameterException('api_key key is absent in configuration');
+        }
+
+    }
+}
