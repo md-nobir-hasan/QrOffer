8f3b1e2bd410f03bbac22178182af55c2887beaa
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-06-21 15:57:25 +0600

    delete

--- app/Http/Controllers/GetOfferController.php
+++ app/Http/Controllers/GetOfferController.php
@@ -45,7 +45,8 @@ class GetOfferController extends Controller
     public function otpcheck($user_id){
         $n['user'] = User::findorFail($user_id);
         if($n['user']){
-            SMS::shoot($n['user']->phone, "Your otp from QROffer ".$n['user']->otp);
+           $sms= SMS::shoot($n['user']->phone, "Your otp from QROffer ".$n['user']->otp);
+           dd($sms);
             return view('pages.otp',$n);
         }else{
             return abort(404,"You don't login again");
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('phone')->unique();
+            $table->string('phone');
             $table->string('otp');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
